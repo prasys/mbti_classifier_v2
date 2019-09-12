4098c2a4b45ff7c8ebe7453f6c777c6f28cb9fe7
Tristan Shah <tristanshah@public-37-63.emich.edu>
2019-09-12 18:55:28 -0400

    model works with new dataset

--- mbti/dataset.py
+++ mbti/dataset.py
@@ -2,8 +2,6 @@ import praw
 from praw.models import MoreComments
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
@@ -13,18 +11,30 @@ reddit = praw.Reddit(user_agent='Comment Extraction (by /u/mbti_)',
                      client_id='TCvLEOtM48aFJg', client_secret='OzboPnt9dvEbbLTexylSlWb-7kc',
                      username='mbti_', password='0p3np0dd00r')
 
-submission = reddit.submission(url='https://www.reddit.com/r/mbti/comments/c43fl8/maybe_the_most_infp_thing_ive_ever_seen/')
+# this is to append the comments from each top post
+dataset = []
 
-submission.author_flair_text
+# gets the comment forest from each of the n# of top posts
+# creates a dataframe of the types as well as their corresponding comment
+for submission in reddit.subreddit('mbti').hot(limit=50):
+    submission = reddit.submission(id=submission.id)
+    count = 0
+    df = pd.DataFrame()
+    submission.comments.replace_more(limit=None)
+    for comment in submission.comments.list():
+        df.loc[count,'comment'] = comment.body
+        df.loc[count,'type'] = comment.author_flair_text
+        count += 1
+    dataset.append(df)
 
-count = 0
-df = pd.DataFrame()
-
-submission.comments.replace_more(limit=None)
-for comment in submission.comments.list():
-    df.loc[count,'comment'] = comment.body
-    df.loc[count,'type'] = comment.author_flair_text
-    count += 1
+# cleans the dataframe by removing comments with no myers briggs type
+dataset = pd.concat(dataset)
+dataset = dataset.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
+dataset['num'] = range(len(dataset))
+dataset.reset_index(level=0, inplace=True)
+dataset.set_index('num')
+dataset = dataset[['type','comment']]
+dataset.to_csv('dataset.csv')
 
 # all mbti types
 typeCount = {
@@ -44,13 +54,13 @@ typeCount = {
     'ISTP' : 0,
     'ISFJ' : 0,
     'ISFP' : 0,
-    'None' : 0,
 }
 
-for i in range(len(df)):
-    typeCount[df.loc[i,'type']] = typeCount.get(df.loc[i,'type'], 0) + 1
+# counts number of times each type made a comment
+for i in range(len(dataset)):
+    typeCount[dataset.loc[i,'type']] = typeCount.get(dataset.loc[i,'type'], 0) + 1
 
+# graphs the number of comments by each type
 plt.bar(range(len(typeCount)), list(typeCount.values()), align='center')
 plt.xticks(range(len(typeCount)), list(typeCount.keys()))
 plt.show()
-# print(typeCount)
--- mbti/mbti.py
+++ mbti/mbti.py
@@ -3,13 +3,16 @@ from io import StringIO
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import chi2
+from numpy import *
 import numpy as np
+
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from nltk import tokenize
 
-df = pd.read_csv('mbti_1.csv')
+df = pd.read_csv('dataset.csv')
 
 df = df[pd.notnull(df['comment'])]
 
@@ -23,13 +26,9 @@ category_id_df = df[['type', 'category_id']].drop_duplicates().sort_values('cate
 category_to_id = dict(category_id_df.values)
 id_to_category = dict(category_id_df[['category_id', 'type']].values)
 
-# imbalanced dataset
-# fig = plt.figure(figsize=(8,6))
-# df.groupby('type')['comment'].count().plot.bar(ylim=0)
-# plt.show()
-
 tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm=None, encoding='latin-1', ngram_range=(1,2), stop_words='english')
 
+# vectorizes each comment in the dataset
 features = tfidf.fit_transform(df['comment']).toarray()
 labels = df.category_id
 
@@ -39,7 +38,28 @@ X_train_counts = count_vect.fit_transform(X_train)
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
+# trains the model
 clf = MultinomialNB().fit(X_train_tfidf, y_train)
 
 # use this to predict text:
-# print(clf.predict(count_vect.transform(["PLACEHOLDER TEXT"])))
+my_input = "I started drinking a bottle of wine close to bedtime. I just wasnt feeling anything though, so I chugged the rest. I went to sleep feeling not too messed up. Some time during the night I woke up have to pee. Instead of stumbling to the toilet, I went to the laundry room and peed in the washing machine. It must have been hard since it's a top-loader. The next day I went to do laundry and the smell was sooo bad! It took me a minute to realize I peed in the washer. I thought that was a dream! Nope. It was real. I did a wash cycle with no clothes in there but a lot of bleach. It smelled fine after that. I'm not really sure why I peed in there. Did I think it would be funny? Did I confuse a white washing machine with a white toilet?? Who knows."
+
+# lines 50-59 seperates my_input into a list of sentances and types each one with the model.
+# Then we list out the models predictions for each sentence.
+paragraph = tokenize.sent_tokenize(my_input)
+types = []
+for sentance in paragraph:
+    type = array2string(clf.predict(count_vect.transform([sentance])))
+    type = type.replace("[","")
+    type = type.replace("]","")
+    type = type.replace("'","")
+    type = type.replace("'","")
+    types.append(type)
+
+print(types)
+
+### optional graph of the datasett
+# imbalanced dataset
+# fig = plt.figure(figsize=(8,6))
+# df.groupby('type')['comment'].count().plot.bar(ylim=0)
+# plt.show()
