165ce75927f727bd6858fdac3a3a01e069ad521c
Tristan Shah <tristanshah@public-37-63.emich.edu>
2019-09-12 15:03:32 -0400

    dataset.py now graphs frequency of mbti types comments

--- mbti/dataset.py
+++ mbti/dataset.py
@@ -1,6 +1,8 @@
 import praw
 from praw.models import MoreComments
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -11,20 +13,44 @@ reddit = praw.Reddit(user_agent='Comment Extraction (by /u/mbti_)',
                      client_id='TCvLEOtM48aFJg', client_secret='OzboPnt9dvEbbLTexylSlWb-7kc',
                      username='mbti_', password='0p3np0dd00r')
 
-submission = reddit.submission(url='https://www.reddit.com/r/mbti/comments/d2uay0/looking_at_you_xnxps/')
-
-submission = reddit.submission(id='d2uay0')
+submission = reddit.submission(url='https://www.reddit.com/r/mbti/comments/c43fl8/maybe_the_most_infp_thing_ive_ever_seen/')
 
 submission.author_flair_text
 
 count = 0
 df = pd.DataFrame()
 
-for top_level_comment in submission.comments:
-    if isinstance(top_level_comment, MoreComments):
-        continue
-    df.loc[count,'comment'] = top_level_comment.body
-    df.loc[count,'type'] = top_level_comment.author_flair_text
+submission.comments.replace_more(limit=None)
+for comment in submission.comments.list():
+    df.loc[count,'comment'] = comment.body
+    df.loc[count,'type'] = comment.author_flair_text
     count += 1
 
-print(df.head())
+# all mbti types
+typeCount = {
+    'ENTJ' : 0,
+    'ENTP' : 0,
+    'ENFJ' : 0,
+    'ENFP' : 0,
+    'ESTJ' : 0,
+    'ESTP' : 0,
+    'ESFJ' : 0,
+    'ESFP' : 0,
+    'INTJ' : 0,
+    'INTP' : 0,
+    'INFJ' : 0,
+    'INFP' : 0,
+    'ISTJ' : 0,
+    'ISTP' : 0,
+    'ISFJ' : 0,
+    'ISFP' : 0,
+    'None' : 0,
+}
+
+for i in range(len(df)):
+    typeCount[df.loc[i,'type']] = typeCount.get(df.loc[i,'type'], 0) + 1
+
+plt.bar(range(len(typeCount)), list(typeCount.values()), align='center')
+plt.xticks(range(len(typeCount)), list(typeCount.keys()))
+plt.show()
+# print(typeCount)
--- mbti/mbti.py
+++ mbti/mbti.py
@@ -11,40 +11,29 @@ from sklearn.naive_bayes import MultinomialNB
 
 df = pd.read_csv('mbti_1.csv')
 
-df = df[pd.notnull(df['posts'])]
+df = df[pd.notnull(df['comment'])]
 
-col = ['type', 'posts']
+col = ['type', 'comment']
 df = df[col]
 
-df.columns = ['type', 'posts']
+df.columns = ['type', 'comment']
 
 df['category_id'] = df['type'].factorize()[0]
 category_id_df = df[['type', 'category_id']].drop_duplicates().sort_values('category_id')
 category_to_id = dict(category_id_df.values)
 id_to_category = dict(category_id_df[['category_id', 'type']].values)
 
+# imbalanced dataset
 # fig = plt.figure(figsize=(8,6))
-# df.groupby('type')['posts'].count().plot.bar(ylim=0)
+# df.groupby('type')['comment'].count().plot.bar(ylim=0)
 # plt.show()
 
 tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm=None, encoding='latin-1', ngram_range=(1,2), stop_words='english')
 
-features = tfidf.fit_transform(df['posts']).toarray()
+features = tfidf.fit_transform(df['comment']).toarray()
 labels = df.category_id
-# print(features.shape)
 
-# N = 2
-# for type, category_id in sorted(category_to_id.items()):
-#     features_chi2 = chi2(features, labels == category_id)
-#     indices = np.argsort(features_chi2[0])
-#     feature_names = np.array(tfidf.get_feature_names())[indices]
-#     unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
-#     bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
-    # print("# '{}':".format(type))
-    # print("  . Most correlated unigrams:\n       . {}".format('\n       . '.join(unigrams[-N:])))
-    # print("  . Most correlated bigrams:\n       . {}".format('\n       . '.join(bigrams[-N:])))
-
-X_train, X_test, y_train, y_test = train_test_split(df['posts'], df['type'], random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(df['comment'], df['type'], random_state = 0)
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(X_train)
 tfidf_transformer = TfidfTransformer()
@@ -52,4 +41,5 @@ X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
 clf = MultinomialNB().fit(X_train_tfidf, y_train)
 
-print(clf.predict(count_vect.transform(["Okay, Makes sense. This is all cool and interesting! If you would like any assistance that you believe Optimize can do, let us know!"])))
+# use this to predict text:
+# print(clf.predict(count_vect.transform(["PLACEHOLDER TEXT"])))
